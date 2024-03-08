478f88ae6dca5c6f60f43edf7b4893df72bad4ce
dipesh617 <dipesh20181111@gmail.com>
2024-03-09 01:02:14 +0530

    first commit

---
+++ src/static/config.js
@@ -0,0 +1,5 @@
+export const api = {
+    url: "https://graphical-pass-backend.vercel.app/"
+    //url:"https://crazy-fish-tights.cyclic.app"
+    // url: "https://graphical-auth-server.onrender.com"
+}
